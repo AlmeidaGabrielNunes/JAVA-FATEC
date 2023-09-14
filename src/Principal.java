dea0245cc89e4cb62ded40826a05cdd1b1b884f2
Gabriel Almieda <almeidagabriel.nunes@gmail.com>
2023-09-14 17:45:43 -0300

    Atividade 1 - Lista 1

--- src/Principal.java
+++ src/Principal.java
@@ -1,9 +1,21 @@
-
+import java.util.Scanner; 
+import java.util.Random; 
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+	       Random rand = new Random(); 
+	       int[] aleatorios = new int[10]; 
+	       for (int c=0; c<=9 ; c++){ 
+	           aleatorios[c] = rand.nextInt(100); 
+	       } 
+	       for(int c=0; c<=9; c++){ 
+	           if(aleatorios[c]%2==0){ 
+	               System.out.println(aleatorios[c]); 
 
+	           } 
+
+	       } 
 	}
 
 }
