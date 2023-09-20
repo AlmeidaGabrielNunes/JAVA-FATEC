c7e6b475d09927b188d76087fab13cc2984dbab9
Nome <almeidagabriel.nunes@gmail.com>
2023-09-20 20:00:16 -0300

    Atividade 2

--- src/Atividade2.java
+++ src/Atividade2.java
@@ -4,10 +4,10 @@ public class Atividade2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		 Scanner ler = new Scanner(System.in); 
-		 int soma; 
+		 int mais; 
 		 int i; 
 		 int c; 
-		 soma = 0; 
+		 mais = 0; 
 		 i = 0; 
 		 c = 0; 
 		 System.out.println("Digite um numero: "); 
@@ -15,12 +15,12 @@ public class Atividade2 {
 		 
 		 while(c<n){ 
 			 if (i%2 !=0){ 
-				 soma = soma + i; 
+				 mais = mais + i; 
 				 c++; 
 			 } 
 		 i++; 
 		} 
-		 System.out.println("O valor e: " + soma); 
+		 System.out.println("O valor e: " + mais); 
 		} 
 		} 
 	
