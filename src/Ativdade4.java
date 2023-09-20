7f3f7c5cb0069d7c5f841a10cb1953e4e0a29861
Nome <almeidagabriel.nunes@gmail.com>
2023-09-20 20:38:53 -0300

    Atividade 4

---
+++ src/Ativdade4.java
@@ -0,0 +1,49 @@
+import java.util.Scanner; 
+
+import java.util.Random; 
+
+public class Ativdade4{ 
+
+    public static void main(String[] args) { 
+
+        Scanner ler = new Scanner(System.in); 
+
+        String senha = "python123"; 
+
+        boolean check = false; 
+
+        String pass; 
+
+        while (check == false){ 
+
+            System.out.println("Digite a senha: "); 
+
+            pass = ler.nextLine(); 
+
+            if(pass.equals(senha)){ 
+
+                check = true; 
+
+            } 
+
+            else{ 
+
+                System.out.println("Senha incorreta. Digite novamente. "); 
+
+            } 
+
+             
+
+        } 
+
+        System.out.println("Sucesso"); 
+
+     
+
+         
+
+        
+
+       } 
+
+            } 
