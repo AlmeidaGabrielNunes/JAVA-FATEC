097d715ab08090faf49b7ea2901788abb7133059
Nome <almeidagabriel.nunes@gmail.com>
2023-09-20 21:11:49 -0300

    Lista 2 Atividade 1

---
+++ src/Lista2Atividade1.java
@@ -0,0 +1,34 @@
+import java.util.Scanner; 
+
+import java.util.Random; 
+
+public class Lista2Atividade1{ 
+
+    public static void main(String[] args) { 
+
+        Scanner ler = new Scanner(System.in); 
+
+       Random rand = new Random(); 
+
+       int[] aleatorios = new int[10]; 
+
+       for (int c=0; c<=9 ; c++){ 
+
+           aleatorios[c] = rand.nextInt(100); 
+
+       } 
+
+       for(int c=0; c<=9; c++){ 
+
+           if(aleatorios[c]%2==0){ 
+
+               System.out.println(aleatorios[c]); 
+
+           } 
+
+       } 
+
+            } 
+
+        } 
+
